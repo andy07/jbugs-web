54b5defbe20c6e6a00fe30304cf1e8b83de82db0
SilviuMihnea <silviu.cucuiet@gmail.com>
2019-05-23 21:24:58 +0300

    Revert "Merge branch 'master' of https://github.com/andy07/jbugs-web"
    This reverts commit 76e3cedb02cb046437d92e5609a24d64d6b05a6a, reversing
    changes made to d1d16fb3e5e37053761780230cff46ab8f1dac59.

--- src/user/models/user.model.ts
+++ src/user/models/user.model.ts
@@ -1,12 +1,11 @@
 export interface User {
-  firstName: string;
-  lastName: string;
-  email: string;
-  mobileNumber: string;
+  firstname: string;
+  lastname: string;
+  age: number;
   roles: Role[];
 }
 export enum Role {
-  edit_bug = 'edit_bug',
-  close_bug = 'close_bug',
-  create_bug = 'create_bug'
+  edit_bug="edit_bug",
+  close_bug="close_bug",
+  create_bug="create_bug"
 }
--- src/user/user-list/user-list.component.ts
+++ src/user/user-list/user-list.component.ts
@@ -2,9 +2,6 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {User} from "../models/user.model";
 import {RestUser} from "../models/restUser";
 import {UserService} from "../service/user.service";
-import {MatTableModule} from '@angular/material/table';
-import {Router} from "@angular/router";
-
 
 @Component({
   selector: 'app-user-list',
@@ -13,27 +10,21 @@ import {Router} from "@angular/router";
 })
 export class UserListComponent implements OnInit {
   public userList: RestUser[];
-  displayedColumns: string[] = ['firstname', 'lastname', 'email', 'mobileNumber', 'status', 'username'];
-
   @Input()
-  public show = true;
+  public show= true;
 
   @Output()
   public output = new EventEmitter<RestUser>();
-  constructor(private userService: UserService, private router: Router) { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
-    this.userService.getAllUsers().subscribe((userList) => {
-      this.userList = userList;
-    });
+    this.userService.getAllUsers().subscribe((userList)=>{
+      this.userList=userList;
+    })
   }
 
-  alertUser(person: RestUser) {
+  alertUser(person:RestUser){
     this.output.emit(person);
-  }
-
-  add(){
-    this.router.navigate(['/add-user']);
   }
 
 
