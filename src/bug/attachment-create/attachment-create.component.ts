66f3f82c85f6588eb8ed668f571fee135fd1ef14
SilviuMihnea <silviu.cucuiet@gmail.com>
2019-06-03 17:39:54 +0300

    Almost everything works

---
+++ src/bug/attachment-create/attachment-create.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-attachment-create',
+  templateUrl: './attachment-create.component.html',
+  styleUrls: ['./attachment-create.component.scss']
+})
+export class AttachmentCreateComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
