2e5a5e8308a844323e0e32dc9c590f804e6eaca9
Florina Diana Stefan <stefandiana0397@gmail.com>
2019-06-07 17:53:27 +0300

    update

--- src/bug/service/bug.service.ts
+++ src/bug/service/bug.service.ts
@@ -39,4 +39,8 @@ export class BugService {
     return this.backendService.get(`/api/bugs/status/no/${status}`);
   }
 
+  public getPostAllAllowedStatus(bugStatus: string): Observable<BugStatus[]> {
+    return this.backendService.get(`/api/bugs/status/${bugStatus}`);
+  }
+
 }
